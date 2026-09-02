b63bfefc29186eb43e5a2ffb62310c0802d43758
Novik Dmitrii <dinovik@rambler.ru>
2026-09-03 00:56:47 +0300

    feat: add routes to dashboard pages

--- src/app/dashboard/items/page.tsx
+++ src/app/dashboard/items/page.tsx
@@ -1,8 +1,6 @@
-import { redirect } from 'next/navigation';
-import ItemTable from '../components/item-table/ItemTable';
+import ItemTable from '../../components/item-table/ItemTable';
 
 export default function DashboardPage() {
-  redirect('/dashboard/items');
   return (
     <div>
       <h1 className="text-2xl font-bold">Dashboard</h1>
---
+++ src/app/dashboard/logs/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+  return (
+    <div>
+      <h3 className="text-2xl font-bold">Logs</h3>
+    </div>
+  );
+}
