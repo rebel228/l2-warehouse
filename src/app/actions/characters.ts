552e16dad2e0aa6b0661c588a28abe81c238ec1c
Novik Dmitrii <dinovik@rambler.ru>
2026-09-06 09:49:59 +0300

    fix: make a separate function for searching a character by name

--- src/app/actions/characters.ts
+++ src/app/actions/characters.ts
@@ -1,9 +1,9 @@
 'use server';
 
 import { db } from '@/lib/db';
-import { characters } from '@/lib/db/schema';
+import { characters, users } from '@/lib/db/schema';
 import { addCharacterSchema } from '@/lib/validations/character.schema';
-import { ilike } from 'drizzle-orm';
+import { eq, ilike } from 'drizzle-orm';
 import { revalidatePath } from 'next/cache';
 
 export type CharacterWithRelations = Awaited<ReturnType<typeof getCharacters>>[number];
@@ -89,6 +89,27 @@ export async function getCharacters() {
     },
     orderBy: (characters, { asc }) => asc(characters.name),
   });
+
+  return result;
+}
+
+export async function searchCharacters(search: string = '') {
+  if (!search || search.length < 1) return [];
+
+  const result = await db
+    .select({
+      id: characters.id,
+      name: characters.name,
+      class: characters.class,
+      user: {
+        id: users.id,
+        username: users.username,
+      },
+    })
+    .from(characters)
+    .leftJoin(users, eq(characters.userId, users.id))
+    .where(ilike(characters.name, `%${search}%`))
+    .limit(10);
 
   return result;
 }
