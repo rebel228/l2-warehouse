de597fb473290117a95a6de33220b35de2d67ca9
Novik Dmitrii <dinovik@rambler.ru>
2026-09-08 23:58:38 +0300

    refactor: make ITEM_TYPES immutable

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const ITEM_GRID_COLS = 'grid-cols-[1fr_0.5fr_1fr_1fr_0.7fr_1fr_1fr]';
 export const GRADES = ['NG', 'D', 'C', 'B', 'A', 'S'] as const;
-export const ITEM_TYPES = ['Weapon', 'Armor', 'Accessory'];
+export const ITEM_TYPES = ['Weapon', 'Armor', 'Accessory'] as const;
 export const DEFAULT_PAGE_SIZE = 20;
 export const CHARACTER_CLASSES = [
   'Gladiator',
