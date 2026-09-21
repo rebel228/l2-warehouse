9754b7099663103995814733c61c7ec51883a000
Novik Dmitrii <dinovik@rambler.ru>
2026-09-21 14:51:20 +0300

    feat: add types for api response

---
+++ src/lib/api/types.ts
@@ -0,0 +1,29 @@
+export interface L2ItemSearchResult {
+  id: number;
+  name: string;
+  type: string;
+  grade: string;
+  weight: number;
+  price: number;
+  iconFile: string;
+}
+
+export interface L2ItemSearchResponse {
+  data: L2ItemSearchResult[];
+  meta: {
+    total: number;
+    limit: number;
+    offset: number;
+  };
+}
+
+export interface L2Item {
+  id: number;
+  name: string;
+  type: string;
+  grade: string;
+  weight: number;
+  price: number;
+  material: string;
+  iconFile: string;
+}
